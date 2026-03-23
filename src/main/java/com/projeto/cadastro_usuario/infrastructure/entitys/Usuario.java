f3dc6637d820f29a7b3ac690c073714f40af52aa
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-23 01:35:54 -0300

    Finalizando

---
+++ src/main/java/com/projeto/cadastro_usuario/infrastructure/entitys/Usuario.java
@@ -0,0 +1,32 @@
+package com.projeto.cadastro_usuario.infrastructure.entitys;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@Table(name = "usuario")
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "nome")
+    private String nome;
+
+}
